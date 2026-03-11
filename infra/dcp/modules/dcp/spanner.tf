556a793a1a2fe5df22c86c5b4ea21dcc483728fd
Gabriel Mechali <gmechali@google.com>
2026-03-11 08:16:02 -0400

    Fix the evaluation of whether to create DCP instance

--- infra/dcp/modules/dcp/spanner.tf
+++ infra/dcp/modules/dcp/spanner.tf
@@ -1,6 +1,6 @@
 resource "google_spanner_instance" "main" {
   count            = var.create_spanner_instance ? 1 : 0
-  name             = var.create_spanner_instance ? "${local.name_prefix}${var.spanner_instance_id}" : "unused"
+  name             = (var.create_spanner_instance && var.spanner_instance_id != "") ? "${local.name_prefix}${var.spanner_instance_id}" : "unused-instance"
   config           = "regional-${var.region}"
   display_name     = "${local.name_prefix}${var.spanner_instance_id}"
   processing_units = var.spanner_processing_units
@@ -10,7 +10,7 @@ resource "google_spanner_instance" "main" {
 
 resource "google_spanner_database" "database" {
   count    = var.create_spanner_db ? 1 : 0
-  instance = var.create_spanner_instance ? google_spanner_instance.main[0].name : (var.spanner_instance_id != "" ? "${local.name_prefix}${var.spanner_instance_id}" : "unused")
+  instance = var.create_spanner_instance ? google_spanner_instance.main[0].name : (var.spanner_instance_id != "" ? "${local.name_prefix}${var.spanner_instance_id}" : "unused-instance")
   name     = "${local.name_prefix}${var.spanner_database_id}"
 
   # Prevent deletion of data
