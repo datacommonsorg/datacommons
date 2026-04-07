c55f013cf535d3510684120e07e8b39060024222
Gabriel Mechali <gmechali@google.com>
2026-04-07 10:02:24 -0400

    Also modify the Edge schema to match the new spanner schema from ingestion pipeline. Verified this works with retrieval

--- packages/datacommons-db/datacommons_db/models/edge.py
+++ packages/datacommons-db/datacommons_db/models/edge.py
@@ -35,24 +35,11 @@ class EdgeModel(Base):
     )
     predicate = sa.Column(String(1024), primary_key=True)
     object_id = sa.Column(String(1024), primary_key=True)
-    object_value = sa.Column(String(OBJECT_VALUE_MAX_LENGTH), nullable=True)
-    object_bytes = sa.Column(sa.LargeBinary(), nullable=True)
-    object_hash = sa.Column(String(64), primary_key=True, nullable=True)
-    provenance = sa.Column(String(1024), primary_key=True, nullable=True)
-    # Use deferred to avoid loading the node data into memory
-    object_value_tokenlist = deferred(
-        sa.Column(Text(), nullable=True)
-    )  #  TOKENLIST is a Spanner type, but represented as String in SQLAlchemy
+    provenance = sa.Column(String(1024), primary_key=True, nullable=False)
 
     # Define relationships to both source and target nodes
     source_node = relationship(
         "NodeModel", foreign_keys=[subject_id], back_populates="outgoing_edges"
-    )
-
-    # Indexes
-    __table_args__ = (
-        # Index for object_value lookups
-        sa.Index("EdgeByObjectValue", "object_value"),
     )
 
     def __repr__(self):
