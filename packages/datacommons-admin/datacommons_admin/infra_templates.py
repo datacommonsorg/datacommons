77dc2bc45b081e83b0142968c565f52de66a13ca
Gabriel Mechali <gmechali@google.com>
2026-05-18 12:38:46 -0400

    Removes the tfvar template

--- packages/datacommons-admin/datacommons_admin/infra_templates.py
+++ packages/datacommons-admin/datacommons_admin/infra_templates.py
@@ -13,32 +13,6 @@
 # limitations under the License.
 
 
-TFVARS_TEMPLATE = """project_id = "{project_id}"
-namespace  = "{namespace}"
-dc_api_key = "{dc_api_key}"
-
-# Optional Spanner configuration (defaults to creating a new instance with dcp-db)
-# dcp_create_spanner_instance = false
-# dcp_spanner_instance_id     = "existing-instance-id"
-dcp_spanner_database_id     = "dcp-db"
-
-# Optional Ingestion Workflow configuration (defaults to true)
-# dcp_deploy_data_ingestion_workflow = false
-# dcp_ingestion_helper_image         = "gcr.io/datcom-ci/datacommons-ingestion-helper:latest"
-
-# Optional GCS input folder (defaults to input)
-# gcs_data_bucket_input_folder = "input"
-
-# Optional CDC Data Job image (defaults to stable)
-# cdc_data_job_image = "gcr.io/datcom-ci/datacommons-data:stable"
-
-# Optional CDC Data Job timeout (defaults to 3600s)
-# cdc_data_job_timeout = "3600s"
-
-# Optional CDC search scope (defaults to base_and_custom)
-# cdc_search_scope = "custom_only"
-"""
-
 REMOTE_STATE_TEMPLATE = """
 ## Remote State Management
 
