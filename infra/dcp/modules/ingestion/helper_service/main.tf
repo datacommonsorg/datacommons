39b800d8751e9ad25b3a82836ca689971eb05c7d
shixiao-coder <shixiao@google.com>
2026-05-28 18:05:16 -0400

    update the timeout for service

--- infra/dcp/modules/ingestion/helper_service/main.tf
+++ infra/dcp/modules/ingestion/helper_service/main.tf
@@ -15,7 +15,7 @@ resource "google_cloud_run_v2_service" "ingestion_helper" {
   deletion_protection = var.stateless_deletion_protection
 
   template {
-    timeout = "600s"
+    timeout = "1800s"
     containers {
       image = var.image
 
