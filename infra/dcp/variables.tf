85224013d29a958e07c45359bb0c43be34a8871c
Yiyuan Chen <yiyche@users.noreply.github.com>
2026-06-15 20:41:16 +0000

    chore: revert default image tags back to 1.0.0 for production consistency

--- infra/dcp/variables.tf
+++ infra/dcp/variables.tf
@@ -316,7 +316,7 @@ variable "datacommons_services_resolve_with_spanner_embeddings" {
 variable "ingestion_preprocessing_job_image" {
   description = "Docker image URL for the data ingestion pre-processing job"
   type        = string
-  default     = "gcr.io/datcom-ci/datacommons-data:latest"
+  default     = "gcr.io/datcom-ci/datacommons-data:1.0.0"
 }
 
 variable "ingestion_preprocessing_job_cpu" {
@@ -378,5 +378,5 @@ variable "ingestion_artifacts_path" {
 variable "ingestion_helper_service_image" {
   description = "Docker image URL for the ingestion support service"
   type        = string
-  default     = "gcr.io/datcom-ci/datacommons-ingestion-helper:stable"
+  default     = "gcr.io/datcom-ci/datacommons-ingestion-helper:1.0.0"
 }
