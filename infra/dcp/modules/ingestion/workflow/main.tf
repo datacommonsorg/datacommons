7ee1e79eeabd12b582f49502c8ae039bc8e3ab61
Christie Ellks <calinc@google.com>
2026-06-18 21:41:46 -0700

    Fix: Pass isBaseDc = false to Dataflow ingestion pipeline in Cloud Workflow (#135)
    In DCP (Data Commons Platform) deployments, the ingestion orchestrator Cloud Workflow launches the Dataflow Flex Template but does not pass the 'isBaseDc' parameter. This causes the Java ingestion pipeline to fall back to its default value of 'isBaseDc = true', which prepends 'dc/base/' to all provenances in the TimeSeries table.
    
    This change explicitly sets 'isBaseDc: false' in the 'launch_params' of the Cloud Workflow to ensure consistency with the 'ingestion_helper' service (which also runs with IS_BASE_DC = false) and prevent incorrect provenance prepending.
    
    TAG=agy
    CONV=b277fd29-905a-414b-a13f-5c026cc258bd

--- infra/dcp/modules/ingestion/workflow/main.tf
+++ infra/dcp/modules/ingestion/workflow/main.tf
@@ -48,6 +48,7 @@ resource "google_workflows_workflow" "ingestion_orchestrator" {
                 tempLocation: '$${input.tempLocation}'
                 stagingLocation: '$${input.tempLocation}'
                 forceCombineNodes: 'true'
+                isBaseDc: 'false'
       - acquire_lock:
           try:
             call: http.post
