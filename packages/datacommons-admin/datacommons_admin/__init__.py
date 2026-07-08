4c979d146d51e91c75a655c1e362b7f0f27e6f4d
yiyche <yiyuanc@google.com>
2026-07-08 14:18:01 -0700

    Make versioning for DCP packages single source (#155)
    * Single-source monorepo package versions using root VERSION file and dynamic runtime metadata
    
    * addressed a few comments
    
    ---------
    
    Co-authored-by: Yiyuan Chen <yiyche@users.noreply.github.com>

--- packages/datacommons-admin/datacommons_admin/__init__.py
+++ packages/datacommons-admin/datacommons_admin/__init__.py
@@ -1,3 +1,5 @@
 """CLI package for administering Data Commons instances in GCP."""
 
-__version__ = "1.1.0"
+import importlib.metadata
+
+__version__ = importlib.metadata.version("datacommons-admin")
--- packages/datacommons-api/datacommons_api/__init__.py
+++ packages/datacommons-api/datacommons_api/__init__.py
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.1.0"
+import importlib.metadata
+
+__version__ = importlib.metadata.version("datacommons-api")
