9fe7e70031dc9b8b4673c108f1be898f9718c1db
Christie Ellks <calinc@google.com>
2026-07-22 23:15:40 -0700

    refactor(terraform): drop v2 from terraform variable name

--- infra/dcp/modules/datacommons_services/variables.tf
+++ infra/dcp/modules/datacommons_services/variables.tf
@@ -73,7 +73,7 @@ variable "resolve_with_spanner_embeddings" {
   type = bool
 }
 
-variable "website_v2_resolve_indicators_target" {
+variable "website_resolve_indicators_target" {
   type        = string
   default     = ""
   description = "Target parameter for indicator v2/resolve calls made by website service"
