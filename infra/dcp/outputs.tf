b1efd1466f02a3e5fba2b7c036ce5383ab09d3ee
Pranjali Sharma <pranjalisharma@google.com>
2026-09-17 22:51:59 +0000

    expose ingestion_artifacts_path outputs.tf (#276)
    * expose ingestion_artifacts_path as a Terraform output
    
    * reword ingestion_artifacts_path output description
    
    Use 'GCS path prefix' instead of 'GCS root directory' to more accurately
    describe the value.
    
    * update description per comment

--- infra/dcp/outputs.tf
+++ infra/dcp/outputs.tf
@@ -70,6 +70,10 @@ output "ingestion_input_path" {
   value       = var.ingestion_input_path
 }
 
+output "ingestion_artifacts_path" {
+  description = "The GCS path to input files in the artifacts bucket."
+  value       = var.ingestion_artifacts_path
+}
 output "network_id" {
   description = "ID of the VPC network"
   value       = module.stack.network_id
