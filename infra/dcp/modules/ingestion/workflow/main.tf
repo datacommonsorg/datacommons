ad49b6792086d541fd6f2151cfcacbb3c2b0a69b
Pranjali Sharma <pranjalisharma@google.com>
2026-09-21 14:13:38 -0400

    update workflow/main.tf to use preprocessor vars from config

--- infra/dcp/modules/ingestion/workflow/main.tf
+++ infra/dcp/modules/ingestion/workflow/main.tf
@@ -2,13 +2,13 @@ locals {
   name_prefix                = var.instance_name != "" ? "${var.instance_name}-" : ""
   should_run_postprocessing  = var.enable_bigquery_postprocessing || var.enable_embeddings_generation
   clean_instance_name_prefix = var.instance_name != "" ? "${replace(lower(var.instance_name), "_", "-")}-" : ""
-  preprocessing_cpu_milli = can(regex("m$", var.preprocessing_job_cpu)) ? tonumber(trimsuffix(var.preprocessing_job_cpu, "m")) : tonumber(var.preprocessing_job_cpu) * 1000
+  preprocessing_cpu_milli    = can(regex("m$", var.preprocessing_config.cpu)) ? tonumber(trimsuffix(var.preprocessing_config.cpu, "m")) : tonumber(var.preprocessing_config.cpu) * 1000
 
   preprocessing_memory_mib = (
-    can(regex("Gi$", var.preprocessing_job_memory)) ? tonumber(trimsuffix(var.preprocessing_job_memory, "Gi")) * 1024 :
-    can(regex("G$", var.preprocessing_job_memory)) ? tonumber(trimsuffix(var.preprocessing_job_memory, "G")) * 1024 :
-    can(regex("Mi$", var.preprocessing_job_memory)) ? tonumber(trimsuffix(var.preprocessing_job_memory, "Mi")) :
-    tonumber(var.preprocessing_job_memory)
+    can(regex("Gi$", var.preprocessing_config.memory)) ? tonumber(trimsuffix(var.preprocessing_config.memory, "Gi")) * 1024 :
+    can(regex("G$", var.preprocessing_config.memory)) ? tonumber(trimsuffix(var.preprocessing_config.memory, "G")) * 1024 :
+    can(regex("Mi$", var.preprocessing_config.memory)) ? tonumber(trimsuffix(var.preprocessing_config.memory, "Mi")) :
+    tonumber(var.preprocessing_config.memory)
   )
 }
 
@@ -41,17 +41,17 @@ resource "google_workflows_workflow" "ingestion_orchestrator" {
     embeddings_timeout                  = var.embeddings_timeout
     clean_instance_name_prefix          = local.clean_instance_name_prefix
     enable_redis_cache_clearing         = var.enable_redis_cache_clearing
-    preprocessing_job_image             = var.preprocessing_job_image
+    preprocessing_job_image             = var.preprocessing_config.image
     preprocessing_cpu_milli             = local.preprocessing_cpu_milli
     preprocessing_memory_mib            = local.preprocessing_memory_mib
-    preprocessing_timeout               = var.preprocessing_job_timeout
-    preprocessing_service_account_email = var.preprocessing_service_account_email
-    bucket_name                         = var.bucket_name
-    ingestion_input_path                = var.ingestion_input_path
-    spanner_instance_id                 = var.spanner_instance_id
-    spanner_database_id                 = var.spanner_database_id
-    enable_spanner_embeddings           = var.enable_spanner_embeddings
-    dc_api_key_secret_version           = var.dc_api_key_secret_version
+    preprocessing_timeout               = var.preprocessing_config.timeout
+    preprocessing_service_account_email = var.preprocessing_config.service_account_email
+    bucket_name                         = var.preprocessing_config.bucket_name
+    ingestion_input_path                = var.preprocessing_config.input_path
+    spanner_instance_id                 = var.preprocessing_config.spanner_instance_id
+    spanner_database_id                 = var.preprocessing_config.spanner_database_id
+    enable_spanner_embeddings           = var.preprocessing_config.enable_spanner_embeddings
+    dc_api_key_secret_version           = var.preprocessing_config.dc_api_key_secret_version
     postprocessing_job_name             = var.postprocessing_job_name
     dataflow_max_workers                = var.dataflow_max_workers
     dataflow_num_workers                = var.dataflow_num_workers
